0d6a4ada99ecfee0638a6a309a3f90b2153fbca2
Jian Cao <jccit@caltech.edu>
2020-02-04 14:56:38 -0800

    Create Send_S3_Files_to_Box_FTP.py
    Codes used to send S3 files to Box FTP

---
+++ Send_S3_Files_to_Box_FTP.py
@@ -0,0 +1,62 @@
+from ftplib import FTP_TLS, error_perm
+import boto3
+import config
+import time
+
+
+## Sending S3 files to Box ---------------------------------------------------------------------------------------------------
+box_path = '/Twitter-Coronavirus/'
+bucketname = 'twitter-coronavirus'
+wait_retry = 60.0  # Seconds before each retry
+wait_next = 3300.0 # Seconds before each update
+
+# Using marker as a starting point (not include) for list_objects request
+marker = '2020/01/31/23/twitter-coronavirus-1-2020-01-31-23-53-43-4e65a429-496b-4f4c-a036-d6509911ca96'
+
+# Function used to connect to Box FTP
+def connect():
+    ftp = FTP_TLS(config.box_host)
+    ftp.debugging = 2
+    ftp.login(config.box_username, config.box_passwd)
+    ftp.cwd(box_path)
+    return ftp
+
+# Connecting to S3 buckets
+s3 = boto3.client('s3', region_name='us-east-2')
+
+# Pushing S3 files to Box folder
+while True:
+    response = s3.list_objects(Bucket=bucketname, Marker=marker)
+    marker_old = marker
+    retry = 0
+    if 'Contents' in response:
+        list_objects = response['Contents']
+        marker = list_objects[-1]['Key'] 
+        ftp = connect()
+        for item in list_objects:
+            key = item['Key']
+            file = s3.get_object(
+                    Bucket=bucketname,
+                    Key=key
+                    )['Body']
+            ftp_command = str('STOR ' + key[14:])
+            try:
+                ftp.storbinary(ftp_command, file)
+            except error_perm as reason:
+                if list_objects.index(item) > 0:
+                    marker = list_objects[list_objects.index(item)-1]['Key']
+                else:
+                    marker = marker_old
+                if str(reason)[:3] != '551':
+                    raise IOError # Need to fix this error before re-try
+                else:
+                    retry = 1
+                    break # Temporary error, re-try uploading
+            file.close()
+        ftp.quit()
+    if retry == 1:
+        print('Retrying...')
+        time.sleep(wait_retry)
+    else:
+        print('Waiting for next update...')
+        time.sleep(wait_next)
