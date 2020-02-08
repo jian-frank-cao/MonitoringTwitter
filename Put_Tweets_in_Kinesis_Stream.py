7898e5d65bb5a9226038573ffc9accc3a4977594
Jian Cao <jccit@caltech.edu>
2020-02-08 12:42:28 -0800

    Update Put_Tweets_in_Kinesis_Stream.py

--- Put_Tweets_in_Kinesis_Stream.py
+++ Put_Tweets_in_Kinesis_Stream.py
@@ -14,11 +14,19 @@ response = client.create_stream(
 """
 
 ## Keywords ------------------------------------------------------------------------------------------------------------------
-keywords = ['#coronavirus','#Coronavirus','coronavirus','#Wuhan','wuhan','#coronavirusoutbreak',
-            '#coronaoutbreak','#facemask','pandemic','#pandemic','#WHO','#2020ncov','#Ncov2019',
-            '#2019Ncov','#wuhanvirus','#wuhanlockdown','#WuhanSARS','#SARS2','#CoronavirusWho',
-            '#Coronavirusoutbreak','#ChinaVirus','#China','#Wuhancoronavirus','#Wuhanpneumonia',
-            '#Health','Coronaoutbreak','2019-nCoV','Virus','SARS','Corona Virus Outbreak']
+keywords = ['provisional ballot', 'voting machine', 'ballot', 'election fraud', 'election manipulation',
+            'illegal voters', 'illegal votes', 'dead voters', 'noncitizen voting', 'noncitizen votes',
+            'illegal voting', 'illegal vote', 'illegal ballot', 'illegal ballots', 'dirty voter rolls',
+            'vote illegally', 'voting illegally', 'voter intimidation', 'voter suppression', 'rigged election',
+            'vote rigging', 'voter fraud', 'voting fraud', 'vote buying', 'vote flipping', 'flipped votes',
+            'voter coercion', 'ballot stuffing', 'ballot box stuffing', 'ballot destruction',
+            'voting machine tampering', 'rigged voting machines', 'voter impersonation', 'election integrity',
+            'election rigging', 'duplicate voting', 'duplicate vote', 'ineligible voting', 'ineligible vote',
+            'absentee ballot', 'mail ballot', 'vote by mail', 'voting by mail', 'early voting',
+            'voter identification', 'voting identification', 'voter id', 'polling place line', 'precinct line',
+            'pollworker', 'poll worker', '@OCRegistrar', '#ocvote2020', '#ocvotecenters2020', '#protect2020',
+            '#OrangeCounty', '#OCVotes', '@LACountyRRCC', '@lacountyrrcc', '#VSAP', '#LAVotes', '#LACounty',
+            '@CASOSvote', '#CAPrimary', '#VoteCalifornia', '#SuperTuesday']
 
 ## Putting tweets in Kinesis stream ------------------------------------------------------------------------------------------
 # Connecting to Twitter API
@@ -26,7 +34,7 @@ api = TwitterAPI(config.consumer_key, config.consumer_secret, config.access_toke
 
 # Connecting to Kinesis stream
 kinesis = boto3.client("kinesis", region_name='us-east-2')
-stream_name = "coronavirus"
+stream_name = "primary-election"
 
 # Endless stream
 while True:
@@ -35,7 +43,7 @@ while True:
         for item in r:
             if 'text' in item:
                 # Randomly assigning partition keys so that tweets can be evenly directed to multiple shards
-                kinesis.put_record(StreamName=stream_name, Data=json.dumps(item), PartitionKey=str(int((time.time()*10000000)%128)))
+                response = kinesis.put_record(StreamName=stream_name, Data=json.dumps(item), PartitionKey=str(int((time.time()*10000000)%128)))
             elif 'disconnect' in item:
                 event = item['disconnect']
                 if event['code'] in [2,5,6,7]:
